51dd4853c83f03d19c59e78f029b7387f474bbc9
milfer0516 <milfer0516@gmail.com>
2024-09-11 21:08:23 -0500

    Modifique el archivo vite.config,ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: "build",
+	},
 	resolve: {
 		alias: {
 			"@": fileURLToPath(new URL("./src", import.meta.url)),
