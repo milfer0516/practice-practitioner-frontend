25fd005b56d4ed3cbd05505298c8684c44f06069
milfer0516 <milfer0516@gmail.com>
2024-09-12 18:40:44 -0500

    Se hacen cambios en el archivo vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	server: {
+		proxy: {
+		'/api': {
+				target: 'http://localhost:4000',
+				changeOrigin: true,
+				secure: false
+			}
+		}
+	},
 	build: {
 		outDir: "build",
 	},
