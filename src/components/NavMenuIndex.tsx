a8e6f74d713191a85620dcb6d2ebd174574337a0
milfer0516 <milfer0516@gmail.com>
2024-09-12 10:54:45 -0500

    Modifico el archivo NavMenuIndex para las rutas de register y login

--- src/components/NavMenuIndex.tsx
+++ src/components/NavMenuIndex.tsx
@@ -1,24 +1,24 @@
-
+import { Link } from "react-router-dom";
 const NavMenuIndex = () => {
-  return (
+	return (
 		<>
 			{/* Registro Y login */}
 			<div className="flex flex-col lg:flex-row w-56 text-center gap-2">
-				<a
-					href="/auth/register"
+				<Link
+					to="/auth/register"
 					className="text-white px-4 py-2 font-medium hover:text-gray-900 bg-fuchsia-700 w-full text-lg"
 				>
 					Registro
-				</a>
-				<a
-					href="/auth/login"
+				</Link>
+				<Link
+					to="/auth/login"
 					className="ml-4 text-white px-7 py-2 text-lg font-medium hover:text-gray-200 bg-fuchsia-700 w-full text-center"
 				>
 					Login
-				</a>
+				</Link>
 			</div>
 		</>
 	);
-}
+};
 
-export default NavMenuIndex
+export default NavMenuIndex;
